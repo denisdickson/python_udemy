99443f365b0476a1282425af6412fe1e88778efe
denisschirmer <denisdschirmer@gmail.com>
2021-01-23 20:19:46 -0300

    nothing changed

--- fundamentosProjetos/area_circulo_v4 copy.py
+++ fundamentosProjetos/area_circulo_v4 copy.py
@@ -1,6 +1,8 @@
 #!python3
 from math import pi 
 
+# coderunner : ctrl + alt + n executarcódigo
+# coderunner : ctrl + alt + m parar execução
+
 raio = input('informe o raio: ')
 print('area do circulo', pi * float(raio) ** 2)
-
--- fundamentosProjetos/area_circulo_v5.py
+++ fundamentosProjetos/area_circulo_v5.py
@@ -1,6 +1,11 @@
 #!python3
 from math import pi 
 
+# coderunner : ctrl + alt + n executarcódigo
+# coderunner : ctrl + alt + m parar execução
+
 raio = input('informe o raio: ')
 print('area do circulo', pi * float(raio) ** 2)
+
+print('Nome do módulo', __name__)
 
