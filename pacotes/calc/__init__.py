7f4e7c53124f65f279339289db0d59362a782e5c
denisdickson <denisdschirmer@gmail.com>
2021-11-11 15:49:48 -0300

    Finalizado capitulo de pacotes com desafio resolve

---
+++ pacotes/calc/__init__.py
@@ -0,0 +1,4 @@
+from pacote1.modulo1 import soma
+from pacote2.modulo1 import subtração
+
+__all__ = ['soma','subtração']
---
+++ pacotes/pacote_v5.py
@@ -0,0 +1,4 @@
+from calc import soma, subtração
+
+print('Soma', soma(3,2))
+print('Subtração', subtração(3,2))
